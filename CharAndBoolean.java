55b7bb3dc99306cbea4fe9a6cbf9d3cf34665df1
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2022-11-03 00:33:32 -0500

    Create CharAndBoolean.java

---
+++ CharAndBoolean.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Char and Unicode
+        char  myChar = 'D';
+        char myUnicodeChar='\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        //Boolean
+        boolean myTrueBooleanValue=true;
+        boolean   myFalseBoolelanVaule=false;
+        boolean  isCustomerOverTwentyOne  =true;
+
+    }
+}
