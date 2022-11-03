8fbd405acad38c5a09c68b323beb77a0bd4419b6
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2022-11-03 00:31:42 -0500

    Create FloatAndDouble.java

---
+++ FloatAndDouble.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float myMinFloatvalue =Float.MIN_VALUE;
+        float myMaxFloatValue =Float.MAX_VALUE;
+        System.out.println("Float Minimum Value = " + myMinFloatvalue);
+        System.out.println("Float Maximum Value = " + myMaxFloatValue);
+        
+        double myMinDoublevalue =Double.MIN_VALUE;
+        double myMaxDoubleValue =Double.MAX_VALUE;
+        System.out.println("Double Minimum Value = " + myMinDoublevalue);
+        System.out.println("Double Maximum Value = " + myMaxDoubleValue);
+
+        int myIntValue =5 / 3;
+        float myFloatValue=5f / 3f;
+        double myDoubleValue=5.00 / 3.00;
+        System.out.println("MyIntValue=" + myIntValue);
+        System.out.println("MyFloatValue=" +myFloatValue);
+        System.out.println("MyDoubleValue="+myDoubleValue);
+
+        double Pounds =5d;
+        double Kilogrmas=Pounds*0.45359237;
+        System.out.println("Pounds to Kilograms =" + Kilogrmas);
+
+
