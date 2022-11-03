3e154d733eb3aef4cf20cde7c70347e722a10faa
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2022-11-03 00:30:34 -0500

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Computer");
+
+        int myFirstnumber = (0 + 3) + (1 + 2);
+        int mySecondnumber = 6;
+        int myThidnumber = 12;
+        int myTotal=myFirstnumber*2;
+        int myLastOne = myTotal-1000;
+        System.out.println(myLastOne);
